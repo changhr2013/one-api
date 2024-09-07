e4c9bd1168837691d7aa96fc9c6d4b2350210ec6
changhr2013 <changhr2013@outlook.com>
2024-09-07 23:51:21 +0800

    [mod] 设置 front_base_url 后，一律不再设置前端路由

--- router/main.go
+++ router/main.go
@@ -4,8 +4,6 @@ import (
 	"embed"
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/songquanpeng/one-api/common/config"
-	"github.com/songquanpeng/one-api/common/logger"
 	"net/http"
 	"os"
 	"strings"
@@ -17,10 +15,10 @@ func SetRouter(router *gin.Engine, buildFS embed.FS) {
 	SetDashboardRouter(context)
 	SetRelayRouter(context)
 	frontendBaseUrl := os.Getenv("FRONTEND_BASE_URL")
-	if config.IsMasterNode && frontendBaseUrl != "" {
-		frontendBaseUrl = ""
-		logger.SysLog("FRONTEND_BASE_URL is ignored on master node")
-	}
+	//if config.IsMasterNode && frontendBaseUrl != "" {
+	//	frontendBaseUrl = ""
+	//	logger.SysLog("FRONTEND_BASE_URL is ignored on master node")
+	//}
 	if frontendBaseUrl == "" {
 		SetWebRouter(router, buildFS)
 	} else {
